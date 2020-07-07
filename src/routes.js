c37a80d4e45aa052ef384d7d967518d0dd0ffa46
LucasAmaral42 <lu.vinicius01@gmail.com>
2020-07-06 23:05:54 -0300

    fix routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const axios = require('axios');
-const Dev = require('../models/Dev');
+const Dev = require('./models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
 
 const routes = Router();
